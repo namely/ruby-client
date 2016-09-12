3c891adbe9a7e5e9b20d616dd0a15076d8b5d5b2
Nazar Khmil <nkhmil@softserveinc.com>
2016-09-12 19:48:45 +0300

    Support 'limit' configuration for pagination.

--- lib/namely.rb
+++ lib/namely.rb
@@ -12,4 +12,16 @@ require "namely/connection"
 require "namely/version"
 
 module Namely
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield configuration
+  end
+
+  class Configuration
+    attr_accessor :limit
+  end
 end
